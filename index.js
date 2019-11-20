428c2c16ecc9b4dc6f1bf8f75e0580ade8d33738
tuanvm <tuanvm@pvcombank.com.vn>
2019-11-20 10:11:11 +0700

    fix

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-let visit = require('unist-util-visit')
-let build = require('./plantuml.js')
+let visit = require("unist-util-visit");
+const plantumlEncoder = require("plantuml-encoder");
 
-const PLUGIN_NAME = 'remark-plantuml'
+const PLUGIN_NAME = "remark-plantuml";
 
-function plantuml () {
-  return function transformer (ast, vFile, next) {
-    visitCodeBlock(ast, vFile)
-    if (typeof next === 'function') { return next(null, ast, vFile) } else { return ast }
-  }
+function plantuml(options) {
+    const opts = options || {};
+    const throwOnError = opts.throwOnError || false;
+
+    return function transformer(ast, vFile, next) {
+        visitCodeBlock(ast, vFile);
+        if (typeof next === "function") {
+            return next(null, ast, vFile);
+        } else {
+            return ast;
+        }
+    };
 }
 
-function visitCodeBlock (ast, vFile) {
-  return visit(ast, 'code', (node, index, parent) => {
-    let {lang, value, position} = node
+function visitCodeBlock(ast, vFile) {
+    return visit(ast, "code", (node, index, parent) => {
+        let { lang, value, position } = node;
 
-    if (lang !== 'plantuml') { return node } // do nothing...
+        if (lang !== "plantuml") {
+            return node;
+        } // do nothing...
 
-    let graphSvgFilename
-    try {
-      graphSvgFilename = build(value)
-      vFile.info(`Building graph from ${lang} code block`, position, PLUGIN_NAME)
-    } catch (error) {
-      vFile.message(error, position, PLUGIN_NAME)
-      return node
-    }
+        // let graphSvgFilename
+        // try {
+        //   graphSvgFilename = build(value)
+        //   vFile.info(`Building graph from ${lang} code block`, position, PLUGIN_NAME)
+        // } catch (error) {
+        //   vFile.message(error, position, PLUGIN_NAME)
+        //   return node
+        // }
 
-    let image = {
-      type: 'image',
-      title: 'PlantUML image',
-      url: graphSvgFilename
-    }
+        options = {};
 
-    parent.children.splice(index, 1, image)
+        options.url = options.url || "http://www.plantuml.com/plantuml/";
+        options.format = options.format || "png";
+        options.base_path = options.base_path || "";
+        options.inline_type = options.inline_type || "plantuml";
 
-    return node
-  })
+        const baseUrl = options.url + options.format + "/";
+
+        const encoded = plantumlEncoder.encode(value);
+
+        plantumlEncoder.title
+
+        let image = {
+            type: "image",
+            title: "PlantUML image",
+            url: baseUrl + encoded
+        };
+
+        parent.children.splice(index, 1, image);
+
+        return node;
+    });
 }
 
-module.exports = plantuml
+module.exports = plantuml;
